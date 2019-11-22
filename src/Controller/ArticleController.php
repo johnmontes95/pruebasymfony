2b24e39260ca386778833f032390f4e5ddc1a942
John Montes <johnmontes1995@gmail.com>
2019-11-22 14:01:36 +0100

    Articulos con comentarios

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -9,9 +9,7 @@ use App\Repository\ArticleRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Psr\Log\LoggerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Twig\Environment;
 
 class ArticleController extends AbstractController
 {
@@ -36,7 +34,7 @@ class ArticleController extends AbstractController
     /**
      * @Route("/news/{slug}", name="article_show")
      */
-    public function show(Article $article, EntityManagerInterface $em){
+    public function show(Article $article){
 
 
 
@@ -48,7 +46,6 @@ class ArticleController extends AbstractController
 
         return $this->render('article/show.html.twig', [
           'article' => $article,
-          'comments' => $lista,
         ]);
 
     }
