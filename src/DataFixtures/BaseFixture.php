ad73f816b6014ce5f1c4a53edfe25d5b8a5c5d93
John Montes <johnmontes1995@gmail.com>
2019-11-25 13:59:20 +0100

    Generador de comentarios añadido

--- src/DataFixtures/BaseFixture.php
+++ src/DataFixtures/BaseFixture.php
@@ -16,6 +16,8 @@ abstract class BaseFixture extends Fixture
     /** @var Generator */
     protected $faker;
 
+    private $referencesindex = [];
+
     public function load(ObjectManager $manager)
     {
         // TODO: Implement load() method.
@@ -34,6 +36,23 @@ abstract class BaseFixture extends Fixture
             // store for usage later as App\Entity\ClassName_#COUNT#
             $this->addReference($className . '_' . $i, $entity);
         }
+    }
+
+
+    protected function getRandomReference(string $className) {
+        if (!isset($this->referencesIndex[$className])) {
+            $this->referencesIndex[$className] = [];
+            foreach ($this->referenceRepository->getReferences() as $key => $ref) {
+                if (strpos($key, $className.'_') === 0) {
+                    $this->referencesIndex[$className][] = $key;
+                }
+            }
+        }
+        if (empty($this->referencesIndex[$className])) {
+            throw new \Exception(sprintf('Cannot find any references for class "%s"', $className));
+        }
+        $randomReferenceKey = $this->faker->randomElement($this->referencesIndex[$className]);
+        return $this->getReference($randomReferenceKey);
     }
 
 }
---
+++ src/DataFixtures/CommentFixture.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Article;
+use App\Entity\Comment;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\DataFixtures\DependentFixtureInterface;
+use Doctrine\Common\Persistence\ObjectManager;
+
+class CommentFixture extends BaseFixture implements DependentFixtureInterface
+{
+
+    protected function loadData(ObjectManager $manager)
+    {
+        // TODO: Implement loadData() method.
+
+        $this->createMany(Comment::class, 100, function (Comment $comment){
+            $comment->setContent(
+                $this->faker->boolean ? $this->faker->paragraph : $this->faker->sentences(2, true)
+            );
+
+            $comment->setAuthorName($this->faker->name);
+            $comment->setCreatedAt($this->faker->dateTimeBetween('-1 months', '-1 seconds'));
+            $comment->setIsDeleted($this->faker->boolean(20));
+            $comment->setArticle($this->getRandomReference(Article::class));
+        });
+
+        $manager->flush();
+    }
+
+    /**
+     * This method must return an array of fixtures classes
+     * on which the implementing class depends on
+     *
+     * @return array
+     */
+    public function getDependencies()
+    {
+        // TODO: Implement getDependencies() method.
+        return [ArticleFixtures::class];
+    }
+}
