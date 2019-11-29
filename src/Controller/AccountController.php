5b073ab347fc960712aef4ff77198604c9f8f595
John Montes <johnmontes1995@gmail.com>
2019-11-29 13:39:25 +0100

    Se ha corregido un error de cache

--- src/Controller/AccountController.php
+++ src/Controller/AccountController.php
@@ -4,7 +4,6 @@ namespace App\Controller;
 
 use Psr\Log\LoggerInterface;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
 /**
@@ -19,6 +18,17 @@ class AccountController extends BaseController
     {
         $logger->debug("Checking account page for " . $this->getUser()->getEmail());
         return $this->render('account/index.html.twig', [
+        ]);
+    }
+
+    /**
+     * @Route("/api/account", name="api_account")
+     */
+    public function accountApi()
+    {
+        $user = $this->getUser();
+        return $this->json($user, 200, [], [
+            'groups' => ['main'],
         ]);
     }
 }
