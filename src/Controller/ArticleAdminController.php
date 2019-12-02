d56d0bbe2a184e6d12c10edfb60f11d8ff0246b8
John Montes <johnmontes1995@gmail.com>
2019-12-02 13:57:38 +0100

    Añadir y editar articulo incorporados

--- src/Controller/ArticleAdminController.php
+++ src/Controller/ArticleAdminController.php
@@ -27,18 +27,15 @@ class ArticleAdminController extends AbstractController
 
         $form->handleRequest($request);
         if($form->isSubmitted() && $form->isValid()){
-            $data = $form->getData();
-            $article = new Article();
-            $article->setTitle($data['title']);
-            $article->setContent($data['content']);
-            $article->setAuthor($this->getUser());
+            /** @var Article $article */
+            $article = $form->getData();
 
             $em->persist($article);
             $em->flush();
 
             $this->addFlash('success', 'Article Created! Knowledge is power!');
 
-            return $this->redirectToRoute('app_homepage');
+            return $this->redirectToRoute('admin_article_list');
         }
 
         return $this->render('article_admin/new.html.twig', [
@@ -47,13 +44,32 @@ class ArticleAdminController extends AbstractController
     }
 
     /**
-     * @Route("admin/article/{id}/edit")
+     * @Route("admin/article/{id}/edit", name="admin_article_edit")
      * @IsGranted("MANAGE", subject="article")
      */
-    public function edit(Article $article)
+    public function edit(Article $article, Request $request, EntityManagerInterface $em)
     {
+        $form = $this->createForm(ArticleFormType::class, $article);
 
-        dd($article);
+        $form->handleRequest($request);
+        if($form->isSubmitted() && $form->isValid()){
+            /** @var Article $article */
+            $article = $form->getData();
+
+            $em->persist($article);
+            $em->flush();
+
+            $this->addFlash('success', 'Article updated! Inaccuracies squashed!');
+
+            return $this->redirectToRoute('admin_article_edit', [
+                'id' => $article->getId()
+            ]);
+        }
+
+        return $this->render('article_admin/edit.html.twig', [
+            'articleForm' => $form->createView()
+        ]);
+
     }
 
 
