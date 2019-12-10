0b6b11be3bede945054d9f294cef4dfb4dd91706
John Montes <johnmontes1995@gmail.com>
2019-12-10 14:00:41 +0100

    Subir ficheros implementado

---
+++ src/Service/UploaderHelper.php
@@ -0,0 +1,48 @@
+<?php
+
+
+namespace App\Service;
+
+
+use Gedmo\Sluggable\Util\Urlizer;
+use Symfony\Component\Asset\Context\RequestStackContext;
+use Symfony\Component\HttpFoundation\File\File;
+use Symfony\Component\HttpFoundation\File\UploadedFile;
+
+class UploaderHelper
+{
+    const ARTICLE_IMAGE = 'article_image';
+
+    private $uploadsPath;
+
+    private $requestStackContext;
+
+    public function __construct(string $uploadsPath, RequestStackContext $requestStackContext)
+    {
+        $this->uploadsPath = $uploadsPath;
+        $this->requestStackContext = $requestStackContext;
+    }
+
+    public function uploadArticleImage(File $file): string
+    {
+        $destination = $this->uploadsPath.'/'.self::ARTICLE_IMAGE;
+        if($file instanceof UploadedFile) {
+            $originalFilename = $file->getClientOriginalName();
+        }else{
+            $originalFilename = $file->getFilename();
+        }
+        $newFileName = Urlizer::urlize(pathinfo($originalFilename, PATHINFO_FILENAME)) . '-' . uniqid() . '.' . $file->guessExtension();
+        $file->move(
+            $destination,
+            $newFileName
+        );
+
+        return $newFileName;
+    }
+
+    public function getPublicPath(string $path): string
+    {
+        return $this->requestStackContext
+        ->getBasePath().'/uploads/' . $path;
+    }
+}
